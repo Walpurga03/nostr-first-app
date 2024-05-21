3edcffc8e3282d412762c8dacc5085d82f6f2806
Walpurga03 <mokiskurve007@gmail.com>
2024-05-21 16:50:34 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/github-pages/", //projekt name anpassen
+  base: "/nostr-first-app/", //projekt name anpassen
 })
